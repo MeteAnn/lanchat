03c89b549aeabbbe3d77770ff36df0c69207e357
Change?mkt=GİTHUB <meeteann@gmail.com>
2025-06-06 03:57:14 +0300

    JWT secret .env dosyasına taşındı, dotenv entegre edildi

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 // routes/auth.js
-
+require('dotenv').config();
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
@@ -11,11 +11,9 @@ router.post('/register', async (req, res) => {
   try {
     const { username, email, password } = req.body;
 
-    // Kullanıcı var mı kontrolü
     const existingUser = await User.findOne({ email });
     if (existingUser) return res.status(400).json({ msg: 'Bu e-posta zaten kayıtlı.' });
 
-    // Şifreyi hashle
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const newUser = new User({
@@ -36,16 +34,14 @@ router.post('/login', async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    // Kullanıcı var mı?
     const user = await User.findOne({ email });
     if (!user) return res.status(404).json({ msg: 'Kullanıcı bulunamadı' });
 
-    // Şifre kontrol
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ msg: 'Geçersiz şifre' });
 
-    // Token oluştur
-    const token = jwt.sign({ id: user._id }, 'süpergizli', { expiresIn: '1d' });
+    
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
 
     res.json({ token, username: user.username });
   } catch (err) {
